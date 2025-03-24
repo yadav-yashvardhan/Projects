17659369879a8c3bae06362b1114e7022ea8adc9
Yash Vardhan Yadav <yashvardhanrao8@gmail.com>
2025-03-24 15:57:58 +0530

    Weather App
    🌦️ Weather App
    A simple and responsive Weather App that fetches real-time weather data using the OpenWeather API.
    
    🔹 Features
    ✅ Search weather by city name 🌍
    ✅ Displays temperature, humidity, and wind speed 🌡️💨
    ✅ Dynamic weather icons based on conditions ☀️🌧️🌫️
    ✅ Fully responsive design using CSS media queries 📱💻
    
    🛠️ Technologies Used
    HTML – Structuring the app
    
    CSS – Styling and responsiveness
    
    JavaScript – Fetching and displaying weather data
    
    🚀 How to Use
    Enter a city name in the search bar.
    
    Click the search button.
    
    Get real-time weather updates instantly!

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-let input = document.getElementById('inputbox');
-let buttons = document.querySelectorAll('button');
+const apiKey = "95dc6479e0bee1515bd3c4968b4af2d0";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-let string="";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'AC'){
-            string="";
-            input.value = string;
-        }
-        else if(e.target.innerHTML =="DEL"){
-            string=string.substring(0,string.length-1);
-            input.value=string;
-        }
-        else{
-            string+=e.target.innerHTML;
-            input.value=string;
-        }
-    })
+async function checkWeather(city){
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+
+    if(response.status == 404){
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
+    } else {
+        var data = await response.json();
+
+        console.log(data);
+    
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML= Math.round(data.main.temp)+ "°c";
+        document.querySelector(".humidity").innerHTML=data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML=data.wind.speed + " Km/h";
+    
+        if(data.weather[0].main == "Clouds"){
+            weatherIcon.src = "clouds.png";
+        }else if(data.weather[0].main == "Clear"){
+            weatherIcon.src = "clear.png";
+    }else if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "clouds.png";}
+        else if(data.weather[0].main == "Rain"){
+            weatherIcon.src = "rain.png";}
+            else if(data.weather[0].main == "Drizzle"){
+                weatherIcon.src = "drizzle.png";}
+                else if(data.weather[0].main == "Mist"){
+                    weatherIcon.src = "mist.png";}
+    
+                    document.querySelector(".weather").style.display="block";
+                    document.querySelector(".error").style.display="none";
+    }
+   
+            }
+
+
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
 })
